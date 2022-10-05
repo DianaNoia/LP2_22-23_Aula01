2a113230c319c25cce84428fdbe7b1e77bb47a1d
Diana Nóia <Diana_noia@hotmail.com>
2022-10-05 14:32:46 +0100

    Exercicio 6
    Exercicio 6, usando acompanhamento dos exercicios resolvidos pelo  professor no github

--- CuteAnimal/Program.cs
+++ CuteAnimal/Program.cs
@@ -6,13 +6,49 @@ namespace CuteAnimal
     {
         static void Main(string[] args)
         {
-            Cat c1 = new Cat(50, "Leonor", Feed.Starving, Mood.Happy);
-            Cat c2 = new Cat(10, "Faisca", Feed.Satisfied, Mood.Grumpy);
-            Cat c3 = new Cat("Pincel");
+            // Declarar os gatos
 
-            Console.WriteLine(c1.Name);
-            Console.WriteLine(c2.Name);
-            Console.WriteLine(c3.Name);
+            Cat c1, c2;
+
+            // Alterar os valores comuns de todos os gatos
+            Cat.MaxEnergy = 300;
+            Cat.EnergyGainAfterSleep = 25;
+            Cat.EnergyLossAfterMeow = 2;
+            Cat.EnergyLossAfterPlay = 14;
+
+            // Criar 2 gatos usando os diferentes construtores 
+
+            c1 = new Cat("Faisca", 10, Feed.Satisfied, Mood.Grumpy);
+            c2 = new Cat("Pincel");
+
+            // Mostrar estado de cada gato antes das ações
+            Console.WriteLine("Estado inicial dos gatos:");
+
+            Console.WriteLine("\tNome={0}, Energia={1}, Alimentação={2}, Disposição={3}",
+                c1.Name, c1.Energy, c1.FeedStatus, c1.MoodStatus);
+
+            Console.WriteLine("\tNome={0}, Energia={1}, Alimentação={2}, Disposição={3}",
+                c2.Name, c2.Energy, c2.FeedStatus, c2.MoodStatus);
+
+            // Gato 1 brinca e mia
+            Console.WriteLine($"{c1.Name} plays");
+            c1.Play();
+            c1.Meow();
+
+            // Gato 2 come e dorme
+
+            Console.WriteLine($"{c2.Name} eats and sleeps");
+            c2.Eat();
+            c2.Sleep();
+
+            // Estado dos gatos após terem efetuado algumas ações
+            Console.WriteLine("Estado final dos gatos, após as suas ações:");
+
+            Console.WriteLine("\tNome={0}, Energia={1}, Alimentação={2}, Disposição={3}",
+                c1.Name, c1.Energy, c1.FeedStatus, c1.MoodStatus);
+
+            Console.WriteLine("\tNome={0}, Energia={1}, Alimentação={2}, Disposição={3}",
+                c2.Name, c2.Energy, c2.FeedStatus, c2.MoodStatus);
         }
     }
 }
